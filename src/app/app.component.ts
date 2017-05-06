f1b247196e49eae0b583458b057970eb1339acc1
danailvidev <danailvidev@gmail.com>
2017-05-07 01:12:42 +0300

    top menu, sass config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,11 @@ export class AppComponent {
   constructor(private greenConfigService: GreenConfigService) {
     let config: GreenConfigSettings = {
       socialIcons: [
-        { imageFile: 'assets/social-icons/facebook.png', alt: 'Facebook', link: 'http://www.facebook.com' },
-        { imageFile: 'assets/social-icons/google.png', alt: 'Google +', link: 'http://www.google.com' },
-        { imageFile: 'assets/social-icons/instagram-256.png', alt: 'Instagram', link: 'http://www.instagram.com' },
-        { imageFile: 'assets/social-icons/linkedin.png', alt: 'LinkedIn', link: 'http://www.LinkedIn.com' },
-        { imageFile: 'assets/social-icons/twitter-256.png', alt: 'Twitter', link: 'http://www.twitter.com' },
+        { imageFile: 'assets/images/social-icons/facebook.png', alt: 'Facebook', link: 'http://www.facebook.com' },
+        { imageFile: 'assets/images/social-icons/google.png', alt: 'Google +', link: 'http://www.google.com' },
+        { imageFile: 'assets/images/social-icons/instagram-256.png', alt: 'Instagram', link: 'http://www.instagram.com' },
+        { imageFile: 'assets/images/social-icons/linkedin.png', alt: 'LinkedIn', link: 'http://www.LinkedIn.com' },
+        { imageFile: 'assets/images/social-icons/twitter-3-256.png', alt: 'Twitter', link: 'http://www.twitter.com' },
       ],
       showLanguageSelector: true,
       showStatusBar: true,
